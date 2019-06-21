a35bdb2ee1a6e094ebf5a6c5716ed861afd4681a
Oliver <ocben1@gmail.com>
2019-06-22 01:31:06 +1000

    added add note functionality

--- src/Board.js
+++ src/Board.js
@@ -1,29 +1,36 @@
 import React, { Component} from 'react'
 import Note from './Note'
+import { FaPlus } from 'react-icons/fa'
 
 class Board extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            notes: [
-                {
-                    id: 0,
-                    note: "Do laundry"
-                },
-                {
-                    id: 1,
-                    note: "Call Grandma"
-                },
-                {
-                    id: 2,
-                    note: "Gym session"
-                }
-            ]
+            notes: []
         }
         this.eachNote = this.eachNote.bind(this)
+        this.add = this.add.bind(this)
         this.update = this.update.bind(this)
         this.remove = this.remove.bind(this)
+        this.nextId = this.nextId.bind(this)
     }
+    add(text) {
+        this.setState(prevState => ({
+            notes: [
+                ...prevState.notes,
+                {
+                    id: this.nextId(),
+                    note: text
+                }
+            ]
+        }))
+    }
+
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
+    }
+
     update(newText, i) {
         console.log('updating note at index', i, newText)
         this.setState(prevState => ({
@@ -53,6 +60,10 @@ class Board extends Component {
         return (
             <div className="board">
                 { this.state.notes.map (this.eachNote) }
+                <button onClick={this.add.bind(null, "New Note")}
+                        id="add">
+                            <FaPlus/>
+                </button>
             </div>
         )
     }
